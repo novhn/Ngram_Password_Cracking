38261937f9d76e94cc0a3ca15ea0b4b640a8437a
John Edwards <je2416@columbia.edu>
2016-04-22 18:40:37 -0400

    refactored code a bit; checks for passwords of length 1 and 2

--- markov_crack_bfs.py
+++ markov_crack_bfs.py
@@ -19,6 +19,19 @@ def process_inputs(argv):
     output_file = argv[3]
     return markov_file, password_file, password_length, output_file
 
+def check_for_hash(password_chars, hashed_passwords, output_file):
+    password = ''
+    for num in password_chars:
+        password+=(chr(int(num)))
+    new_hash = hashlib.md5(password).hexdigest()
+    sys.stdout.write('%s\r' % password)
+    sys.stdout.flush()
+    while new_hash in hashed_passwords:
+        print "password found:" + password + "," + new_hash
+	with open(output_file,'ab') as output_data:
+	    output_data.write(password+'::::'+new_hash+'\n')
+	hashed_passwords.remove(new_hash)
+
 
 def markov_crack(markov_file, password_file, password_length, output_file):
     with open(password_file, 'rb') as password_data:
@@ -50,6 +63,7 @@ def markov_crack(markov_file, password_file, password_length, output_file):
         for i in range(ngram_len, int(password_length)+1):
             for chars in leading_chars:
                 stack = [(chars, 0)]
+                check_for_hash(chars, hashed_pwd, output_file)
                 while len(stack) > 0:
                     # get current password to construct
                     curr = stack.pop()
@@ -57,7 +71,7 @@ def markov_crack(markov_file, password_file, password_length, output_file):
                     curr_index = curr[1]
                     if curr_index == len(ngrams):
                         continue  # if we've reached end of prob_ngrams list, continue
-                    if len(curr_pass) > i:
+                    if len(curr_pass) >= i:
                         continue  # if curr password is max length, continue
                     for x in xrange(curr_index, len(ngrams)):
                         # look for matching ngram
@@ -69,20 +83,7 @@ def markov_crack(markov_file, password_file, password_length, output_file):
                             # add new password to stack
                             stack.append((new_curr, 0))
                             # try cracking password, if it works, terminate loop
-                            new_pass = ''
-                            for num in new_curr:
-                                new_pass+=(chr(int(num)))
-                            new_hash = hashlib.md5(new_pass).hexdigest()
-                            if len(new_pass) == ngram_len:
-                                print new_pass
-                            sys.stdout.write('%s\r' % new_pass)
-                            sys.stdout.flush()
-                            while new_hash in hashed_pwd:
-                                print "password found:" + new_pass + "," + new_hash
-
-                                with open(output_file,'ab') as output_data:
-                                    output_data.write(new_pass+'::::'+new_hash+'\n')
-                                hashed_pwd.remove(new_hash)
+                            check_for_hash(new_curr, hashed_pwd, output_file)
                             if len(hashed_pwd) == 0:
                                 end = time.time()
                                 print end-start
