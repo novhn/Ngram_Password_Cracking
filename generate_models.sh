2c45c78384ab5a04deb457e55ff0e75db638b9ed
Wesley Chu <wwc2113@columbia.edu>
2016-04-27 13:59:13 -0400

    removing unigram from test suite

--- generate_models.sh
+++ generate_models.sh
@@ -1,7 +1,5 @@
-python ngram_freq_analyzer.py 1 ./password_lists/top_10000_pwd.txt ./markov_models/hmm_top10000_uni_exist.txt existing
 python ngram_freq_analyzer.py 2 ./password_lists/top_10000_pwd.txt ./markov_models/hmm_top10000_bi_exist.txt existing
 python ngram_freq_analyzer.py 3 ./password_lists/top_10000_pwd.txt ./markov_models/hmm_top10000_tri_exist.txt existing
-python ngram_freq_analyzer.py 1 ./password_lists/top_10000_pwd.txt ./markov_models/hmm_top10000_uni_all.txt all
 python ngram_freq_analyzer.py 2 ./password_lists/top_10000_pwd.txt ./markov_models/hmm_top10000_bi_all.txt all
 python ngram_freq_analyzer.py 3 ./password_lists/top_10000_pwd.txt ./markov_models/hmm_top10000_tri_all.txt all
 
