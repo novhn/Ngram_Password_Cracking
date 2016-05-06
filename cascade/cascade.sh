e70f98201b0a022e7826fe8ed041713bfa389442
John Edwards <jedwards@samsungaccelerator.com>
2016-05-06 18:15:14 -0400

    adding data from first cascade test

--- cascade/cascade.sh
+++ cascade/cascade.sh
@@ -4,4 +4,6 @@ echo 'target 50, exists'
     python markov_crack_bfs_cascade.py ../markov_models/hmm_top10000_bi_exist.txt uncracked_by_dictionary.txt 16 cracked_by_50_exists.txt uncracked_by_50_exists.txt 50
 echo 'target 100, exists'
     python markov_crack_bfs_cascade.py ../markov_models/hmm_top10000_bi_exist.txt uncracked_by_50_exists.txt 16 cracked_by_100_exists.txt uncracked_by_100_exists.txt 100
+echo 'target 500, exists'
+    python markov_crack_bfs_cascade.py ../markov_models/hmm_top10000_bi_exist.txt uncracked_by_100_exists.txt 16 cracked_by_500_exists.txt uncracked_by_500_exists.txt 500
 
