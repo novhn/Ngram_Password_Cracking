72d1927c31c709b7e595d1bb2d3c1c954e82723f
John Edwards <jedwards@samsungaccelerator.com>
2016-05-08 10:42:48 -0400

    update shell script to take cl input

--- cascade/cascade.sh
+++ cascade/cascade.sh
@@ -1,5 +1,7 @@
+VAR1=$1
+
 echo 'dictionary crack'
-    python dictionary_cracker.py ../password_lists/top_10000_pwd.txt singles_hashes.txt cracked_by_dictionary.txt uncracked_by_dictionary.txt
+    python dictionary_cracker.py ../password_lists/top_10000_pwd.txt $1 cracked_by_dictionary.txt uncracked_by_dictionary.txt
 echo 'target 50, exists'
     python markov_crack_bfs_cascade.py ../markov_models/hmm_top10000_bi_exist.txt uncracked_by_dictionary.txt 16 cracked_by_50_exists.txt uncracked_by_50_exists.txt 50
 echo 'target 100, exists'
