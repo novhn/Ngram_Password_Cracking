70ed14594ce0345a95647816df01dd5946f1c9f2
John Edwards <je2416@columbia.edu>
2016-05-08 15:14:53 -0400

    reorganize cascade results; add trigram cascade script

---
+++ cascade/tri_cascade.sh
@@ -0,0 +1,11 @@
+VAR1=$1
+
+echo 'dictionary crack'
+    python dictionary_cracker.py ../password_lists/top_10000_pwd.txt $1 cracked_by_dictionary.txt uncracked_by_dictionary.txt
+echo 'target 50, exists'
+    python markov_crack_bfs_cascade.py ../markov_models/hmm_top10000_tri_exist.txt uncracked_by_dictionary.txt 16 cracked_by_50_exists.txt uncracked_by_50_exists.txt 50
+echo 'target 100, exists'
+    python markov_crack_bfs_cascade.py ../markov_models/hmm_top10000_tri_exist.txt uncracked_by_50_exists.txt 16 cracked_by_100_exists.txt uncracked_by_100_exists.txt 100
+echo 'target 500, exists'
+    python markov_crack_bfs_cascade.py ../markov_models/hmm_top10000_tri_exist.txt uncracked_by_100_exists.txt 16 cracked_by_500_exists.txt uncracked_by_500_exists.txt 500
+
